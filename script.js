e0433bd9a3fe8685e6bd3a2b69d7e59b95967929
AdelinPamant <adelin.pamant@yahoo.com>
2019-01-27 11:23:12 +0200

    updated scripts

---
+++ script.js
@@ -0,0 +1,215 @@
+var keys = ["A0", "Bb0", "B0", "C1", "Db1", "D1", "Eb1", "E1", "F1",
+    "Gb1", "G1", "Ab1", "A1", "Bb1", "B1", "C2", "Db2", "D2", "Eb2",
+    "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2", "C3", "Db3", "D3",
+    "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", "C4", "Db4",
+    "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", "C5",
+    "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5", "B5",
+    "C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6", "A6", "Bb6",
+    "B6", "C7", "Db7", "D7", "Eb7", "E7", "F7", "Gb7", "G7", "Ab7", "A7",
+    "Bb7", "B7", "C8"];
+var audioType = "-mp3";
+var currentInstrument = [];
+var audio;
+var volume;
+
+document.addEventListener("drag", function (event) {
+    
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+    if (event.target.className == "note") {
+        console.log(12);
+        playNote(event.target);
+    }
+}, false);
+
+window.onload = function() {
+    try {
+        window.AudioContext = window.AudioContext||window.webkitAudioContext;
+        audio = new AudioContext();
+        // if (!audio.createGain) audio.createGain = audio.createGainNode;
+        // if (!audio.createDelay) audio.createDelay = audio.createDelayNode;
+        // if (!audio.createScriptProcessor) audio.createScriptProcessor = audio.createJavaScriptNode;
+        volume = audio.createGain();
+        volume.gain.minValue = 0;
+        volume.gain.maxValue = 100;
+        volume.gain.defaultValue = 50;
+    }
+    catch (e) {
+        console.log("Eroare la creare audio");
+    }
+    volumeControl = document.getElementById("volumeSlider");
+    volumeControl.addEventListener("input", function() {
+        volume.gain.value = this.value;
+    }, false);
+    setInstrument("acoustic_grand_piano");
+    createNotes();
+    // createSequencer();
+    // createOptions();
+
+};
+
+function base64ToArrayBuffer(base64) { // Transform base64 to ArrayBuffer for Web Audio
+    var binaryString =  window.atob(base64);
+    var len = binaryString.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+function createSequencer() {
+    sequencerLocation = document.getElementById("timelineGrid");
+}
+
+function createHtmlNotes() {
+    var container = document.getElementById("musicalNotes");
+    var divElement;
+
+    for (var i = 0; i < keys.length; i++) {
+        divElement = document.createElement("div");
+        container.appendChild(divElement);
+    }
+}
+
+function createNotes() {
+    var noteLocation = document.getElementById("musicalNotes").getElementsByTagName("div");
+    
+    for (var i = 0; i < 40; i++){
+        noteLocation[i].textContent = keys[i];
+        noteLocation[i].className = "note";
+        noteLocation[i].addEventListener("click", function (event) {
+            playNote(event.target);
+        });
+    }
+}
+
+// function setInstrument(instrumentName) {
+//     var xmlhttp = new XMLHttpRequest();
+//     var tempInstrument;
+//     // deci noi avem un for care trece prin niste date. si noi vrem a folosim datele din for inauntrul functiei audio.decodeAudioData care returneaza n promise si function(buffer) e aplicat ca un fel de then pe el
+//     // xmlhttp.onload = function() {
+//     //     if (this.readyState == 4 && this.status == 200) {
+//     //         tempInstrument = JSON.parse(this.responseText);
+//     //         keys = [];
+//     //         for (var note in tempInstrument) keys.push(note);
+//     //         var tempKeys = keys;
+//     //         var i = 0;
+//     //         return tempKeys.reduce((promise, item) => {
+//     //             return promise.then((result) => {
+//     //                 return audio.decodeAudioData(base64ToArrayBuffer(item), 
+//     //                 function(buffer) {
+//     //                     temp = new Object(buffer);
+//     //                     currentInstrument[note] = temp;
+//     //                     console.log(currentInstrument[1]);
+                        
+//     //                     console.log(currentInstrument[note]);
+//     //                     var source = audio.createBufferSource();
+//     //                     source.buffer = currentInstrument[note];
+//     //                     source.connect(audio.destination);
+//     //                     source.start();
+//     //                 }).then(result);
+//     //             }, Promise.resolve());    
+//     //         });
+//     //     }
+//     // };
+
+//     xmlhttp.onload = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             tempInstrument = JSON.parse(this.responseText);
+//             keys = [];
+//             for (var note in tempInstrument) keys.push(note);
+//             var tempKeys = keys;
+//             // var i = 0;
+
+//             // console.log(base64ToArrayBuffer(tempInstrument[tempKeys[i]]));
+//             tempKeys.reduce((promise, item) => {
+//                 return promise.then((result) => {
+//                     audio.decodeAudioData(base64ToArrayBuffer(tempInstrument[note]), 
+//                     function(buffer) {
+//                         temp = new Object(buffer);
+//                         console.log(note);
+//                         currentInstrument[note] = temp;
+//                         console.log(currentInstrument);
+
+//                         console.log(currentInstrument[note]);
+//                         // var source = audio.createBufferSource();
+//                         // source.buffer = currentInstrument[note];
+//                         // source.connect(audio.destination);
+//                         // source.start();
+//                     });
+//                 });    
+//             }, Promise.resolve());
+//         }
+//     };
+//     xmlhttp.open("GET", "data/" + instrumentName + audioType + ".json", true);
+//     xmlhttp.send();
+// }
+
+
+function setInstrument(instrumentName) {
+    var xmlhttp = new XMLHttpRequest();
+    var tempInstrument;
+
+    xmlhttp.onload = function () {
+
+        createHtmlNotes();
+
+        if (this.readyState === 4 && this.status === 200) {
+            tempInstrument = JSON.parse(this.responseText);
+            keys = [];
+            
+            for (const note in tempInstrument) {
+                audio.decodeAudioData(base64ToArrayBuffer(tempInstrument[note]),
+                    function (buffer) {
+                        let temp = new Object(buffer);
+                        currentInstrument[note] = temp;
+                        // var source = audio.createBufferSource();
+                        // source.buffer = currentInstrument[note];
+                        // source.connect(audio.destination);
+                        // source.start();
+                    });
+            }
+        }
+    };
+    xmlhttp.open("GET", "data/" + instrumentName + audioType + ".json", true);
+    xmlhttp.send();
+}
+
+
+function play() {
+
+}
+
+function playNote(target) {
+    var source = audio.createBufferSource();
+    source.buffer = currentInstrument[target.textContent];
+    source.connect(audio.destination);
+    source.start(0);
+}
+
+function addNote(noteName, sequencerTime) {
+    // audio[0].src = currentInstrument[noteName];
+}
+
+function createOptions() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            currentInstrument = JSON.parse(this.responseText);
+            for (var i = 0; i < currentInstrument.length; i++) {
+                for (var data in currentInstrument) {
+                    var selectBox = document.getElementById("instrumentBox");
+                    var option = document.createElement("option");
+                    option.text = "Kiwi"; // regex
+                    option.value = data[i];
+                    selectBox.add(option);
+                }
+                
+            }
+        }
+    };
+    xmlhttp.open("GET", "data/instruments.json", true);
+    xmlhttp.send();
+}
